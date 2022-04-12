0f736804649f3a846fea0e05ac8215cee4a5f675
Jalen Pastor <jalenpastor@gmail.com>
2022-04-11 20:16:21 -0700

    Final commit for week 2

--- Wordle Clone/Assets/Scripts/Type.cs
+++ Wordle Clone/Assets/Scripts/Type.cs
@@ -8,6 +8,9 @@ public class Type : MonoBehaviour
     public GameObject[] letters;
     public string letterInput = string.Empty;
     public string testWord = "world";
+    public int placeHolder = 0;
+    public int numTries = 0;
+    public int currentTry;
     public int it = 0;
     // Start is called before the first frame update
     void Start()
@@ -27,24 +30,36 @@ public class Type : MonoBehaviour
         {
             it--;
         }
-        else if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Backspace) && it < 29)
+        else if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Backspace) && it < 4)
         {
             it++;
         }
+        else if (it == 4 && Input.GetKeyDown(KeyCode.Return) && placeHolder < 30)
+        {
+            letters[4].GetComponent<InputField>().DeactivateInputField();
+            it = 0;
+            numTries++;
+            placeHolder += 5;
 
-        letters[it].GetComponent<InputField>().ActivateInputField();
+            for (int i = placeHolder - 5; i < placeHolder; i++)
+            {
+                letters[i].GetComponent<InputField>().GetComponent<Image>().color = new Color(0.3f, 0.3f, 0.3f, 1);
+            }
+        }
+
+        if (numTries < 6)
+        {
+            letters[it + placeHolder].GetComponent<InputField>().ActivateInputField();
+        }
+        else if (numTries == 6)
+        {
+            // TODO: Game Over
+        }
 
         if (it >= 0)
         {
             // TODO: convert to uppercase
             // Debug.Log(letters[it-1].GetComponent<InputField>().text[0]);
         }
-
-        if (it%5 == 0)
-        {
-            
-        }
-        
-        Debug.Log(it);
     }
 }
