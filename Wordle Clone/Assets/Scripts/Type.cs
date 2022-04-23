76da699e3bff47a5d2091a48101b106969f5a188
Jalen Pastor <jalenpastor@gmail.com>
2022-04-23 04:48:56 -0700

    Deliverable draft
    Added game over texts, implemented dictionary

--- Wordle Clone/Assets/Scripts/Type.cs
+++ Wordle Clone/Assets/Scripts/Type.cs
@@ -7,19 +7,28 @@ public class Type : MonoBehaviour
 {
     public KeyCode[] desiredKeys = {KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, 
     KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z};
+    public string[] dictionary = {"adult", "beach", "chair", "dream", "enemy", "faith", "glass", "horse", "image", "judge", "knife", "light", "major", "nurse", "owner", 
+    "phone", "queen", "radio", "scope", "taste", "unity", "value", "white", "young"};
+    public GameObject gameOverText;
+    public bool gameOver = false;
     public GameObject[] letters;
     public string stringInput; 
     private string letterInput = string.Empty;
-    private string testWord = "voice";
-    private int placeHolder = 0;
+    public string puzzleWord;
+    private int randomNum;
+    public int placeHolder = 0;
     private int currentTry;
     public int it = 0;
     public int numTries = 0;
     public int numCorrectLetters = 0;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomNum = Random.Range(0, 23);
+        puzzleWord = dictionary[randomNum];
+        gameOverText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -52,39 +61,28 @@ public class Type : MonoBehaviour
         }
         else if (it == 4 && Input.GetKeyDown(KeyCode.Return) && placeHolder < 30)
         {
-            for (int i = placeHolder; i < placeHolder + 5; i++)     // fill the stringInput with characters
+            // fill the stringInput with characters
+            for (int i = placeHolder; i < placeHolder + 5; i++)
             {
                 stringInput += letters[i].GetComponent<InputField>().text[0];
             }
 
             CheckString();
 
-            if (numCorrectLetters == 5)
+            if (gameOver == false)
             {
-                // TODO: You win
+                letters[4].GetComponent<InputField>().DeactivateInputField();
+                it = 0;
+                numTries++;
+                placeHolder += 5;
+                stringInput = "";
             }
-
-            letters[4].GetComponent<InputField>().DeactivateInputField();
-            it = 0;
-            numTries++;
-            placeHolder += 5;
-            stringInput = "";
         }
 
         // Activate the next input field
-        if (numTries < 6)
+        if (numTries < 6 && gameOver == false)
         {
             letters[it + placeHolder].GetComponent<InputField>().ActivateInputField();
-        }
-        else if (numTries == 6)
-        {
-            // TODO: Game Over
-        }
-
-        if (it >= 0)
-        {
-            // TODO: convert to uppercase
-            // Debug.Log(letters[it-1].GetComponent<InputField>().text[0]);
         }
     }
 
@@ -92,10 +90,11 @@ public class Type : MonoBehaviour
     private void CheckString()
     {
         numCorrectLetters = 0;
+
         // Check for completely right or completely wrong inputs
-        for (int i = 0; i < testWord.Length; i++)
+        for (int i = 0; i < puzzleWord.Length; i++)
         {
-            if (stringInput[i] == testWord[i])
+            if (stringInput[i] == puzzleWord[i])
             {
                 letters[i + placeHolder].GetComponent<InputField>().GetComponent<Image>().color = Color.green;
                 numCorrectLetters++;
@@ -106,15 +105,32 @@ public class Type : MonoBehaviour
             }
         }
 
-        for (int i = 0; i < testWord.Length; i++)
+        // Check for half-correct inputs
+        for (int i = 0; i < puzzleWord.Length; i++)
         {
-            for (int j = 0; j < testWord.Length; j++)
+            for (int j = 0; j < puzzleWord.Length; j++)
             {
-                if (stringInput[i] == testWord[j] && stringInput[i] != testWord[i])
+                if (stringInput[i] == puzzleWord[j] && stringInput[i] != puzzleWord[i])
                 {
                     letters[i + placeHolder].GetComponent<InputField>().GetComponent<Image>().color = Color.yellow;
                 }
             }
+        }
+
+        // If all letters are completely correct, win. If number tries > 6, lose.
+        if (numCorrectLetters == 5)
+        {
+            gameOver = true;
+            gameOverText.GetComponent<Text>().color = Color.green;
+            gameOverText.GetComponent<Text>().text = "You win!";
+            gameOverText.SetActive(true);
+        }
+        else if (numTries == 5)
+        {
+            gameOver = true;
+            gameOverText.GetComponent<Text>().color = Color.red;
+            gameOverText.GetComponent<Text>().text = "You lose!";
+            gameOverText.SetActive(true);
         }
     }
 }
